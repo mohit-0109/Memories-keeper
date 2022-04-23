b8c1932ae9ababb4c2e5367a0b4b8b499513adfb
mohit-0109 <mohit.nandargi@gmail.com>
2022-04-23 11:26:49 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://mohit01:Mohit123123@cluster0.owyuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+app.get('/',(req,res) => {
+  res.send("hi to memoreiws");
+ });
+ 
+ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
